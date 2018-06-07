fecd284808b8ad4a6e5a86fa2188240257ad35a6
WL ssh <william.lechelle@umontreal.ca>
2018-06-07 12:34:54 -0400

    initial commit that works on octal15

--- train.py
+++ train.py
@@ -5,9 +5,13 @@ import numpy as np
 import os
 os.environ['CUDA_VISIBLE_DEVICES']='0'
 #Input training files from benchmarks/FB15K/ folder.
+
+kb = 'RV15M' # FB15K, DefIE
+model = "TransE" # DistMult
+
 con = config.Config()
-#True: Input test files from the same folder.
-con.set_in_path("./benchmarks/FB15K/")
+# Currently FB15K is in benchmarks/FB15K instead of data/FB15K
+con.set_in_path("/u/wujieche/Projects/data/"+kb+"/")
 
 con.set_test_triple_classification(True)
 con.set_work_threads(8)
@@ -22,15 +26,15 @@ con.set_rel_neg_rate(0)
 con.set_opt_method("SGD")
 
 #Models will be exported via tf.Saver() automatically.
-con.set_export_files("./res/model.vec.tf", 0)
+con.set_export_files("models/{}-{}_model.vec.tf".format(model, kb), 0)
 #Model parameters will be exported to json files automatically.
-con.set_out_files("./res/embedding.vec.json")
+con.set_out_files("models/{}-{}_embeddings.vec.json".format(model, kb))
 #Initialize experimental settings.
 con.init()
 #Set the knowledge embedding model
-con.set_model(models.TransE)
+con.set_model(getattr(models, model)) # models.TransE
 #Train the model.
 con.run()
 #To test models after training needs "set_test_flag(True)".
-con.test()
+#con.test()
 
